acc1ba83918c535a971e2dc9e7edff6123e12fc1
Yashpal Gawali <gawaliyashpal@gmail.com>
2025-08-20 17:48:47 +0530

    20 Aug sec commit

--- src/Components/Competency/CompetencyComponent.jsx
+++ src/Components/Competency/CompetencyComponent.jsx
@@ -35,7 +35,6 @@ export default function CompetencyComponent () {
             getCompetencyById()
         }
     }, [id] ) 
-       
 
     function onSubmit(values) {
         setIsDisabled(true)
