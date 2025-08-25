ca10ef54431f3e169887649f349ac4a25260913d
Yashpal Gawali <gawaliyashpal@gmail.com>
2025-08-25 17:42:34 +0530

    25 Aug sec commit

--- src/Components/api/apiClient.js
+++ src/Components/api/apiClient.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// For Localhost
-// export const apiClient = axios.create({
-//     baseURL : 'http://localhost:7878/'
-// })
+// For External Tomcat
+export const apiClient = axios.create({
+    baseURL : 'http://localhost:8080/'
+})
 
 // For Local Tomcat
-export const apiClient = axios.create({
-    baseURL : 'http://localhost:7878/trainingtrackerrest/'
-})
+// export const apiClient = axios.create({
+//     baseURL : 'http://localhost:7878/trainingtrackerrest/'
+// })
 
 // For Tomcat 10
 // export const apiClient = axios.create({
